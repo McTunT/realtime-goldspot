bc5a8cf1e03643321bf118d77e9e8d2e34faed48
Teerayuth Thanakornyingyong <teerayuth.james@gmail.com>
2019-07-04 11:02:36 +0700

    remove file

--- src/components/GoldJiwelryBid.js
+++
@@ -1,6 +0,0 @@
-import React, { useState, useReducer } from "react";
-import { useFetchs } from "./Goldsport";
-
-function init(initialG965B) {
-  return { stateG965B: initialG965B };
-}
--- src/components/GoldSpotReducer.js
+++
@@ -1,5 +0,0 @@
-const bid_asso = "BIDASSO";
-const offer_asso = "OFFERASSO";
-const bid_jiwelry = "BIDJIWELRY";
-const offer_jiwelry = "OFFERJIWELRY";
-const interest = "INTEREST";
